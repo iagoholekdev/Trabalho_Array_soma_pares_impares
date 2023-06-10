b9c2e192f04b5e2ffefa0eb44cb2e9e8148843ee
iagoholekdev <augustinhocarrara43@gmail.com>
2023-06-09 22:10:25 -0300

    ajustes array

--- TrabalhoJava/src/consts/utils.java
+++ TrabalhoJava/src/consts/utils.java
@@ -12,7 +12,7 @@ public class utils {
     public final static String totalPares = "Total de elementos pares: ";
     public final static String totalImpares =  "Total de elementos ímpares: ";
 
-    public final static String percentualPares = "Percentual de elementos pares: %.2f ";
+    public final static String percentualPares = "Percentual de elementos pares: %.2f \n";
     public final static String percentualImpares = "Percentual de elementos ímpares: %.2f ";
     public final static String mensagemPadraoMatriz =  "Digite os valores da matriz: ";
     public final static String matriz = "Matriz: ";
